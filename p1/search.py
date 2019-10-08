0eae41ccb35100e7bbc3262e2a6d0e07db669070
U160709040 <yagmurmutluer9@gmail.com>
2019-10-08 12:30:18 +0300

    terminal file added ym

--- p1/search.py
+++ p1/search.py
@@ -1,8 +1,10 @@
 import re
-filename = "graph.txt"
+import sys
+#filename = "graph.txt"
+graph = 'C:\\Users\\bk\\Desktop\\ceng3511\\p1\\graph.txt'
 
 # open the file for reading
-filehandle = open(filename, 'r')
+filehandle = open(sys.argv[1], 'r')
 while True:
     # read a single line
     line = filehandle.readline()
@@ -13,3 +15,28 @@ while True:
 
 # close the pointer to that file
 filehandle.close()
+#
+#
+# # visits all the nodes of a graph (connected component) using BFS
+# def bfs_connected_component(graph, start):
+#     # keep track of all visited nodes
+#     explored = []
+#     # keep track of nodes to be checked
+#     queue = [start]
+#
+#     # keep looping until there are nodes still to be checked
+#     while queue:
+#         # pop shallowest node (first node) from queue
+#         node = queue.pop(0)
+#         if node not in explored:
+#             # add node to list of checked nodes
+#             explored.append(node)
+#             neighbours = graph[node]
+#
+#             # add neighbours of node to queue
+#             for neighbour in neighbours:
+#                 queue.append(neighbour)
+#     return explored
+#
+#
+# bfs_connected_component(graph, 'A')  # returns ['A', 'B', 'C', 'E', 'D', 'F', 'G']
